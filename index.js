1f3ff51b8e032e5d776191469d04154445509737
facundo <facuc0503@gmail.com>
2022-04-13 21:20:01 -0300

    subo adaptacion a media query

---
+++ index.js
@@ -0,0 +1,5 @@
+const menuBurguer = document.getElementById("burguer");
+const menu = document.getElementById("menu");
+menuBurguer.addEventListener("click",function(){
+    menu.classList.toggle("show")
+});
